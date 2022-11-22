e6d0d2c4af6ae20f9cbb6292fcf1b1c87153bf5e
Koki Tanaka <mail@gojiteji.com>
2022-11-22 18:00:16 +0900

    Update txt2img.py

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -228,7 +228,7 @@ def main():
 
     precision_scope = autocast if opt.precision=="autocast" else nullcontext
     with torch.no_grad():
-        with precision_scope("cuda"):
+        with precision_scope(True):
             with model.ema_scope():
                 tic = time.time()
                 all_samples = list()
